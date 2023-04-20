1cc2f6dcbd6c8a7645e1b183738033f876661d4e
Ing Zhen <111691855+codingwithiz@users.noreply.github.com>
2023-04-20 23:06:48 +0800

    Add files via upload

---
+++ Tuto 6/src/Tuto6.java
@@ -0,0 +1,39 @@
+public class Tuto6 {
+    /*
+    Q1:
+    peek
+
+    Q2:
+    s2.push(s1.pop()); s1[zero,one] s2[two] s3[]
+    s3.push(s1.pop()); s1[zero] s2[two] s3[one]
+    s1.pop(); s1[] s2[two] s3[one]
+    s1.push(s2.pop()); s1[two] s2[] s3[one]
+    s2.push(s3.pop()); s1[two] s2[one] s3[]
+    s2.push(s1.pop()); s1[] s2[one,two] s3[]
+
+    Q3:
+    (a) 1-2-3
+    Yes. push(1) -> pop() [1]-> push(2) -> pop() [2] -> push(3) -> pop() [3]
+
+    (b) 2-3-1
+    Yes. push(1) -> push(2) -> pop() [2] -> push(3) -> pop() [3] -> pop() [1]
+
+    (c) 3-2-1
+    Yes. push(1) -> push(2) -> push(3) -> pop() [3] -> pop() [2] -> pop() [1]
+
+    (d) 1-3-2
+    Yes. push(1) -> pop() [1] -> push(2) -> push(3) -> pop() [3] -> pop() [2]
+
+    Q4:
+    a) a+b*c = abc*+
+    b) a*b-c/d = ab*cd/-
+    c) a+(b*c+d)/e = abc*d+e/+
+
+    Q5:
+    a) a b + c* = (a+b)*c
+    b) a b c + * = a*(b+c)
+
+    Q6:
+    D. All of the above
+     */
+}
